477624d6d3c6a18fd6e61584d61b29c4a6a8b0a3
Adam Abundis <adamjabundis@gmail.com>
2022-07-07 17:58:34 -0700

    refactor: create game board component

---
+++ src/components/GameBoard.vue
@@ -0,0 +1,50 @@
+<script lang="ts">
+import GameCard from "./GameCard.vue";
+import { defineComponent, PropType } from "vue";
+import { Card, SelectedCard } from "../interfaces";
+
+export default defineComponent({
+  name: "GameBoard",
+  components: { GameCard },
+  props: {
+    cardList: {
+      type: Array as PropType<Array<Card>>,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    turns: {
+      type: Number,
+      required: true,
+    },
+  },
+  emits: ["flip-card"],
+  setup(props, context) {
+    const selectCard = (payload: SelectedCard) => {
+      context.emit("flip-card", payload);
+    };
+
+    return {
+      selectCard,
+    };
+  },
+});
+</script>
+<template>
+  <TransitionGroup tag="section" class="game-board" name="shuffle-cards">
+    <GameCard
+      v-for="card in cardList"
+      :value="card.value"
+      :visible="card.visible"
+      :key="`${card.value}-${card.variant}`"
+      @select-card="selectCard"
+      :card="card"
+    />
+  </TransitionGroup>
+  <div class="info-container">
+    <h2 class="status">{{ status }}</h2>
+    <h2 class="turns">Turns: {{ turns }}</h2>
+  </div>
+</template>
