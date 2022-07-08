253fd792dc99139cfd522c77b974fb27adb7901d
Adam Abundis <adamjabundis@gmail.com>
2022-07-07 17:57:55 -0700

    refactor: create footer component

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,12 @@
+<template>
+  <footer>
+    <p>Made by <a href="https://adamabundis.xyz">Adam Abundis</a></p>
+    <p>
+      <a
+        href="https://www.flaticon.com/free-icons/camping"
+        title="camping icons"
+        >Camping icons created by iconixar - Flaticon</a
+      >
+    </p>
+  </footer>
+</template>
